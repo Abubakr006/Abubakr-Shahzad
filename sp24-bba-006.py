7e61490e4b26e5730d9bf8e0221097fa3b46da70
Abubakr006 <abubakrshahzad57@gmail.com>
2025-02-26 22:47:51 +0500

    Assignment # 1

---
+++ sp24-bba-006.py
@@ -0,0 +1,32 @@
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Display several numbers
+... print(4,5,6,7,8)
+4 5 6 7 8
+>>> # Solve and show the summation of 64 + 32
+... print("64 + 32 =", 64 + 32)
+64 + 32 = 96
+>>> # Solve and show summation using variables
+... x=64
+>>> y=32
+>>> sum=x+y
+>>> print("sum=",sum)
+sum= 96
+>>> # Print the word 'Lucky' inside a variable
+... s = "Lucky"
+... print(s)
+SyntaxError: multiple statements found while compiling a single statement
+>>> s="lucky"
+>>> print(s)
+lucky
+>>> # Print today's date in the format "Today is 2/2/2016"
+... day=2
+>>> month=2
+>>> year=2025
+>>> print("today is",day,"/",month,"/",year)
+today is 2 / 2 / 2025
+>>> # Ask user for phone number and display it
+... phone_number=input("03006238080")
+03006238080
+>>> print("my phone number is:",phone_number)
+my phone number is: 
